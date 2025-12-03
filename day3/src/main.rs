f6b0f4f564cb208d46b3c5e4f77f7ad922e8fa90
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2025-12-03 09:46:09 -0500

    feat: day3b in Rust

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,19 +2,22 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+const NUMBER_OF_DIGITS: usize = 12;
+
 fn main() -> io::Result<()> {
     let path = Path::new("input.txt");
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
-    let mut sum: i32 = 0;
+    let mut sum: u64 = 0;
 
     for line in reader.lines() {
         let line = line?;
-        let digits = convert_string_to_digits(line);
-        let (first_number, second_number) = find_highest_pair(&digits);
+        let digits = convert_string_to_digits(&line);
+        let highest_digits = find_highest_digits(&digits, NUMBER_OF_DIGITS);
 
-        sum += first_number as i32 * 10;
-        sum += second_number as i32;
+        for (i, digit) in highest_digits.iter().enumerate() {
+            sum += *digit as u64 * 10_u64.pow((NUMBER_OF_DIGITS - i - 1) as u32)
+        }
     }
 
     println!("{}", sum);
@@ -22,16 +25,29 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn convert_string_to_digits(s: String) -> Vec<u8> {
+fn convert_string_to_digits(s: &String) -> Vec<u8> {
     s.bytes().map(|b| b - b'0').collect()
 }
 
-fn find_highest_pair(digits: &Vec<u8>) -> (u8, u8) {
-    let first_number = find_max_of_vector(&digits[0..digits.len() - 1].to_vec());
-    let index_of_first_number = digits.iter().position(|&x| x == first_number).unwrap();
-    let second_number = find_max_of_vector(&digits[index_of_first_number + 1..].to_vec());
+fn find_highest_digits(digits: &Vec<u8>, number_of_digits: usize) -> Vec<u8> {
+    let mut start = 0;
+    let mut end = digits.len() - (number_of_digits - 1);
+    let mut result = Vec::new();
 
-    (first_number, second_number)
+    for _i in 0..number_of_digits {
+        let number = find_max_of_vector(&digits[start..end].to_vec());
+
+        start += &digits[start..end]
+            .iter()
+            .position(|&x| x == number)
+            .unwrap()
+            + 1;
+        end += 1;
+
+        result.push(number);
+    }
+
+    result
 }
 
 fn find_max_of_vector(digits: &Vec<u8>) -> u8 {
